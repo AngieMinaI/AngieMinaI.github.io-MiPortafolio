974656c29561c6dccd3c102922c4d1021f0df954
Angie Mina Ishuiza <107209336+AngieMinaI@users.noreply.github.com>
2025-08-22 12:14:01 -0500

    act enlace CV

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ export default function Home() {
       <div className="home-content">
         <h1>Bienvenido a mi portafolio!<br/>Soy Angie Mina Ishuiza</h1>
         <p>Ingeniera de Software </p>
-        <a href="/CV-Angie-Mina-Ishuiza.pdf" download className="cv-btn">
-          Descargar CV <span className="icon">⬇</span>
+        <a href={`${import.meta.env.BASE_URL}CV-Angie-Mina-Ishuiza.pdf`} download className="cv-btn">
+            Descargar CV <span className="icon">⬇</span>
         </a>
       </div>
     </section>
